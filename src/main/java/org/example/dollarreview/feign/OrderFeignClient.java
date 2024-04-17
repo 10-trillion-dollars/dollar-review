ba308eea4322274be81b19e42327bf65091c2895
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-17 11:30:19 +0900

    :sparkles: Feign 요청 실패를 대비한 retry 구현

--- src/main/java/org/example/dollarreview/feign/OrderFeignClient.java
+++ src/main/java/org/example/dollarreview/feign/OrderFeignClient.java
@@ -1,8 +1,11 @@
 package org.example.dollarreview.feign;
 
+import feign.FeignException.FeignClientException;
 import java.util.List;
 import org.example.dollarreview.domain.order.OrderDetail;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +16,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface OrderFeignClient {
 
     @GetMapping("/users/{userId}/products/{productId}")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     Long countByUserIdAndProductId(@PathVariable Long userId, @PathVariable Long productId);
     @GetMapping("/order/users/{userId}/products/{productId}")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     String checkOrderState(@PathVariable Long userId,@PathVariable Long productId);
 
     @GetMapping("/users/{userId}/products/{productId}/orders")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     List<OrderDetail> getOrderDetails(@PathVariable Long userId, @PathVariable Long productId);
 
     @PostMapping("/orders/orderDetail/reviewState")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     void saveOrderDetailReviewedState(@RequestBody OrderDetail orderDetail);
 }
--- src/main/java/org/example/dollarreview/feign/ProductFeignClient.java
+++ src/main/java/org/example/dollarreview/feign/ProductFeignClient.java
@@ -1,8 +1,11 @@
 package org.example.dollarreview.feign;
 
 
+import feign.FeignException.FeignClientException;
 import org.example.dollarreview.domain.product.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProductFeignClient {
 
     @GetMapping("/products/{productId}")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     Product getProduct(@PathVariable("productId") Long productId);
 
 }
